72ed6f2e35de939d65847b8737b2be97f4f46472
Saul Fautley <saul@beyondweb.co.za>
2019-03-28 23:17:11 +0200

    Removed git support because it's slow af.

--- functions/_pure_prompt_first_line.fish
+++ functions/_pure_prompt_first_line.fish
@@ -1,13 +1,13 @@
 function _pure_prompt_first_line \
     --description 'Print contextual information before prompt.'
 
-    if not type -fq git  # exit if git is not available
-        return 1
-    end
+    #if not type -fq git  # exit if git is not available
+    #    return 1
+    #end
 
     set --local prompt (_pure_print_prompt \
                             (_pure_prompt_ssh) \
-                            (_pure_prompt_git) \
+                            #(_pure_prompt_git) \
                             (_pure_prompt_command_duration)
                         )
     set --local prompt_width (_pure_string_width $prompt)
@@ -17,14 +17,14 @@ function _pure_prompt_first_line \
     if test $pure_begin_prompt_with_current_directory = true
         set prompt_components \
                 (_pure_prompt_current_folder $prompt_width) \
-                (_pure_prompt_git) \
+                #(_pure_prompt_git) \
                 (_pure_prompt_ssh) \
                 (_pure_prompt_command_duration)
     else
         set prompt_components \
                 (_pure_prompt_ssh) \
                 (_pure_prompt_current_folder $prompt_width) \
-                (_pure_prompt_git) \
+                #(_pure_prompt_git) \
                 (_pure_prompt_command_duration)
     end
 
